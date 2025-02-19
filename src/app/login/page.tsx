05910d1a48b53bc74e586570933f3dce115bbed9
Maks-Nata <mixcim20121984@gmail.com>
2025-02-19 02:58:04 +0200

    added home.css and login.css

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,72 +1,76 @@
- "use client";
-
- import {FormEvent, useState} from "react";
- import { useRouter } from "next/navigation";
- import { setCookie } from "cookies-next";
-
-
- export default function LoginPage() {
-     const [username, setUsername] = useState("");
-     const [password, setPassword] = useState("");
-     const [error, setError] = useState("");
-     const router = useRouter();
-
-     const handleLogin = async (e: FormEvent) => {
-         e.preventDefault();
-         setError("");
-
-         try {
-             const res = await fetch("https://dummyjson.com/auth/login", {
-                 method: "POST",
-                 headers: { "Content-Type": "application/json" },
-                 body: JSON.stringify({ username, password }),
-             });
-
-             if (!res.ok) throw new Error("Неправильний логін або пароль");
-
-             const data = await res.json();
+"use client";
+import "./login.css"
+import {FormEvent, useState} from "react";
+import {useRouter} from "next/navigation";
+import {setCookie} from "cookies-next";
 
 
-             setCookie("token", data.token, { maxAge: 60 * 60 * 24, path: "/" });
-             setCookie("user", JSON.stringify({ id: data.id, name: data.firstName }), {
-                 maxAge: 60 * 60 * 24,
-                 path: "/",
-             });
+export default function LoginPage() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+
+    const handleLogin = async (e: FormEvent) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const res = await fetch("https://dummyjson.com/auth/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username, password}),
+            });
+
+            if (!res.ok) throw new Error("Неправильний логін або пароль");
+
+            const data = await res.json();
 
 
-             router.push("/users");
-         } catch (err: unknown) {  //
-             if (err instanceof Error) {
-                 setError(err.message);
-             } else {
-                 setError("Сталася невідома помилка");
-             }
-         }
-     };
+            setCookie("token", data.token, {maxAge: 60 * 60 * 24, path: "/"});
+            setCookie("user", JSON.stringify({id: data.id, name: data.firstName}), {
+                maxAge: 60 * 60 * 24,
+                path: "/",
+            });
 
-          return (
-      <div>
 
-             <h1>Вхід</h1>
-             <form onSubmit={handleLogin}>
-                 <input
-                     type="text"
-                     placeholder="Логін"
-                     value={username}
-                     onChange={(e) => setUsername(e.target.value)}
-                     required
-                 />
-                 <input
-                     type="password"
-                     placeholder="Пароль"
-                     value={password}
-                     onChange={(e) => setPassword(e.target.value)}
-                     required
-                 />
-                 <button type="submit">Увійти</button>
-             </form>
-             {error && <p style={{ color: "red" }}>{error}</p>}
-         </div>
-     );
- }
+            router.push("/users");
+        } catch (err: unknown) {  //
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError("Сталася невідома помилка");
+            }
+        }
+    };
+
+    return (
+        <div className="login-container">
+
+                <img width="200" height="200" src="https://img.icons8.com/pulsar-line/100/kitchen-room.png" alt="kitchen-room"/>
+                <h1 className="login-title">Вхід</h1>
+
+
+
+            <form className="login-form" onSubmit={handleLogin}>
+                <input className="login-input"
+                       type="text"
+                       placeholder="Логін"
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)}
+                       required
+                />
+                <input className="login-input"
+                       type="password"
+                       placeholder="Пароль"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                       required
+                />
+                <button className="login-button" type="submit">Увійти</button>
+            </form>
+            {error && <p style={{color: "red"}}>{error}</p>}
+        </div>
+    );
+}
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Link from "next/link";
+import "./home.css";
 
 export default function Home() {
-  return (
+    return (
 
-      <div><h1>Ласкаво просимо</h1> <p>Щоб отримати доступ до рецептів Вам потрібно автентифікуватися</p> <Link href="/login">
-          <button>Увійти</button>
-      </Link></div>
-  );
+        <div className="home-container">
+            <div className="home-about"><img width="200" height="200" src="https://img.icons8.com/dotty/80/chef-cooking.png"
+                                             alt="chef-cooking"/>
+                <p className="home-history">Я знайомилася з різними людьми і просила дати мені їх кращі рецепти хтось робив, це
+                    відразу хтось сказав, що
+                    зробить це потім.Тому був створений цей сайт, для того щоб люди змогли в зручний для них час додати свій
+                    улюблений рецепт.Рецепти постійно поновлюються.</p>
+            </div>
+            <h1 className="home-title">Улюблені страви від різних людей</h1>
+
+            <h2 className="home-description">Ласкаво просимо до нашого додатку!</h2>
+            <div className="home-container-button"><p className="home-text">Щоб отримати доступ
+                до рецептів Вам потрібно автентифікуватися</p> <Link href="/login">
+                <button className="home-button">Увійти</button>
+            </Link></div>
+            </div>
+    );
 }
